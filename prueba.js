140d067b8ea1815b4a2416d8a1ecacfca0974608
rekeber <69129780+rekeber@users.noreply.github.com>
2021-02-26 14:30:58 -0600

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,7 @@
+function suma(){
+	var primer_numero, segundo_numero;
+	primer_numero=document.getElementById('primer_numero').value;
+	segundo_numero=document.getElementById('segundo_numero').value;
+
+	alert(parseInt(primer_numero)+parseInt(segundo_numero));
+}
